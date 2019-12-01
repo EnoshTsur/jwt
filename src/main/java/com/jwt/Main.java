0a75a6bbe61baac9446c3ba1ca091ce9574bb393
EnoshTs <enosh.tsur@haaretz.co.il>
2019-12-02 01:37:14 +0200

    added http site in comment

--- src/main/java/com/jwt/Main.java
+++ src/main/java/com/jwt/Main.java
@@ -8,5 +8,6 @@ public class Main {
     public static void main(String[] args) {
         String jwt = JWTDemo.createJWT("123", "zooki", "enosh", TimeUnit.SECONDS.toMillis(3));
         System.out.println(jwt);
+        // https://developer.okta.com/blog/2018/10/16/token-auth-for-java
     }
 }
