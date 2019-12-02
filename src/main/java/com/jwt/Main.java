90e1ce3d1d6f1429fe876fba1eb88f56e39c486f
EnoshTs <enosh.tsur@haaretz.co.il>
2019-12-02 10:06:15 +0200

    using jwt

--- src/main/java/com/jwt/Main.java
+++ src/main/java/com/jwt/Main.java
@@ -7,6 +7,5 @@ public class Main {
     public static void main(String[] args) {
         String jwt = JWTHandler.createJWT("123", "zooki", "enosh", TimeUnit.SECONDS.toMillis(3));
         System.out.println(jwt);
-        // https://developer.okta.com/blog/2018/10/16/token-auth-for-java
     }
 }
